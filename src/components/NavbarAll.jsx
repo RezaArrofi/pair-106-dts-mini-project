f473aacb876340c9d106435363bfc98545955155
RezaArrofi <arrofireza23@gmail.com>
2022-07-21 20:40:10 +0700

    ganti navbar

--- src/components/NavbarAll.jsx
+++ src/components/NavbarAll.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
 const NavbarAll = () => {
   return (
     <>
     <AppBar position="static" sx={{backgroundColor:"red"}}>
       <Toolbar>
-        {/* <IconButton
+        <IconButton
           size="large"
           edge="start"
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
         >
-          <MenuIcon /> */}
-        {/* </IconButton> */}
+          <Menu />
+        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           News
         </Typography>
